d09585e3cf7e7c2b545e55c6e4d8f935bc2d45d7
Christian Concha <christianconchaflores@gmail.com>
2020-08-29 00:27:29 -0400

    DB Authorization parameters and BOT token. Config file

---
+++ config/auth2.py
@@ -0,0 +1,6 @@
+token = '1234567890:blAblablabla...' #Bot Token
+username = 'username' #DB username
+password = 'password' #DB password
+dns = 'db_server.sub.domain/DB_name' #dns in the form of: server.subdomain.domain/DB_name
+port = xxxx #port number
+encoding = 'UTF-8'
